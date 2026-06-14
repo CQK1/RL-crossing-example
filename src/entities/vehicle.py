26a2c50a46e8e20aa7f5f28f1868a8eb131af68d
Kai <qiankai.chen@uleth.ca>
2026-06-14 17:33:45 -0600

    chore: added my classes

--- src/entities/vehicle.py
+++ src/entities/vehicle.py
@@ -29,14 +29,3 @@ class Vehicle:
 
         if self.speed == 0.0:
             self.waiting_time += dt
-
-class Pedestrian:
-    def __init__(self, start_pos = 0):
-        self.position = start_pos
-        self.waiting_time = 0
-    
-    def move(self, is_green):
-        if is_green:
-            self.position += 1
-        else:
-            self.waiting_time += 1
